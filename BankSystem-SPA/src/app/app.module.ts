6ac81ae9d568236db1393f5568d8063761e300a3
Dmitry Bobrov <dmitry.bobrov@yellow.systems>
2020-03-02 23:10:49 +0300

    Add front-end initialization & clients table

--- BankSystem-SPA/src/app/app.module.ts
+++ BankSystem-SPA/src/app/app.module.ts
@@ -2,13 +2,56 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ClientsListComponent } from './clients-list/clients-list.component';
+import { AddClientComponent } from './add-client/add-client.component';
+import { ClientFormComponent } from './client-form/client-form.component';
+import { EditClientComponent } from './edit-client/edit-client.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { RouterModule, Routes } from '@angular/router'
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientsListComponent,
+    AddClientComponent,
+    ClientFormComponent,
+    EditClientComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    RouterModule.forRoot([
+      {
+        path: 'clients',
+        component: ClientsListComponent,
+        data: { title: 'Clients List' }
+      },
+      {
+        path: '',
+        redirectTo: '/clients',
+        pathMatch: 'full'
+      },
+      {
+        path: 'clients/create',
+        component: AddClientComponent,
+        data: { title: 'Add Client' }
+      },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
